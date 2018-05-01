6eb47b1ad8d876fce0b21ff86283be73c79a411a
Malin Yasantha <maliny@jkintranet.com>
2018-05-01 12:15:45 +0530

    test controller added

---
+++ WebApiTemplate.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Reflection;
+
+namespace WebApiTemplate.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string jsonStr;
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Json\test.json");
+            using (StreamReader r = new StreamReader(path))
+            {
+                jsonStr = r.ReadToEnd();
+            }
+            return jsonStr;
+        }
+    }
+}
